ac9266e925ce8b4815d27f7a9cdc081658cd6315
JAYESH OM SAHARAVAT <jayeshsaharavat1@gmail.com>
2026-02-21 23:14:35 +0530

    Create Q42.c

---
+++ DAY21/Q42.c
@@ -0,0 +1,22 @@
+//Given the head of a singly linked list, return the middle node of the linked list.
+//If there are two middle nodes, return the second middle node.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* middleNode(struct ListNode* head) {
+    struct ListNode *slow = head;
+    struct ListNode *fast = head;
+    
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    
+    return slow;
+}c
