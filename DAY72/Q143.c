d9e30bcb978324164d6d77103489bbbcb411f8af
JAYESH OM SAHARAVAT <jayeshsaharavat1@gmail.com>
2026-04-13 22:00:07 +0530

    Create Q143.c

---
+++ DAY72/Q143.c
@@ -0,0 +1,25 @@
+// Given a string s consisting of lowercase letters, find the first repeated character in the string. A character is considered repeated if it appears more than once, and among all such characters, the one whose second occurrence has the smallest index should be returned.
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+    scanf("%s", s);
+
+    int visited[26] = {0};  // for 'a' to 'z'
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        int index = s[i] - 'a';
+
+        if (visited[index] == 1) {
+            printf("%c", s[i]);
+            return 0;
+        }
+
+        visited[index] = 1;
+    }
+
+    printf("-1");
+    return 0;
+}
