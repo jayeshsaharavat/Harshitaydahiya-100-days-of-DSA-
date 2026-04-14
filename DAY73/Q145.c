42a1d3ae721d1338a1fbfa5f6e49e7b802364ff3
JAYESH OM SAHARAVAT <jayeshsaharavat1@gmail.com>
2026-04-14 22:13:10 +0530

    Create Q145.c

---
+++ DAY73/Q145.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+char firstNonRepeating(char *s) {
+    int freq[26] = {0};
+
+    // Step 1: Count frequency
+    for (int i = 0; s[i] != '\0'; i++) {
+        freq[s[i] - 'a']++;
+    }
+
+    // Step 2: Find first non-repeating
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (freq[s[i] - 'a'] == 1) {
+            return s[i];
+        }
+    }
+
+    return '$';
+}
+
+int main() {
+    char s[1000];
+
+    // Input
+    scanf("%s", s);
+
+    // Output
+    printf("%c\n", firstNonRepeating(s));
+
+    return 0;
+}
